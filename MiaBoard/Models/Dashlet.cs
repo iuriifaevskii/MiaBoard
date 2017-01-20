efdf90d93a83bff067be5f6fd43a2e51e0e73f57
Kostetskiy <ya.kosteckiy@gmail.com>
2017-01-20 15:52:59 +0200

    added few fields for model

--- MiaBoard/Models/Dashlet.cs
+++ MiaBoard/Models/Dashlet.cs
@@ -11,18 +11,21 @@ namespace MiaBoard.Models
 
         public string Title { get; set; }
 
-        public string Body { get; set; }
-
-        public Dashboard Dashboard { get; set; }
+        public string TextType { get; set; }
 
         public int DashboardId { get; set; }
 
         public DataSource DataSource { get; set; }
-
         public int DataSourceId { get; set; }
 
         public string Sql { get; set; }
 
         public string Styles { get; set; }
+        public Dashboard Dashboard { get; set; }
+        
+        public string TopSubLevel { get; set; }
+        
+        public string BottomSubTitle { get; set; }
+
     }
 }
