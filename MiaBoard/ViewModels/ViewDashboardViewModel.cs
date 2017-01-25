592e73f9f184a97403ecb362423298a422cbe99b
Kostetskiy <ya.kosteckiy@gmail.com>
2017-01-25 15:27:03 +0200

    added fields for a View

--- MiaBoard/ViewModels/ViewDashboardViewModel.cs
+++ MiaBoard/ViewModels/ViewDashboardViewModel.cs
@@ -10,6 +10,11 @@ namespace MiaBoard.ViewModels
     {
         public Dashboard Dashboard { get; set; }
         public IDictionary<int, string> DashletsSqlResult { get; set; }
+
         public List<Dashlet> Dashlets { get; set; }
+
+        public List<Dashlet> DashletsFirstCol { get; set; }
+        public List<Dashlet> DashletsSecondCol { get; set; }
+        public List<Dashlet> DashletsThirdCol { get; set; }
     }
 }
