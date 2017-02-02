bd09c5c300763907214227772c3322bbbdd8e500
Iurii Faevskii <urfaevskii@gmail.com>
2017-02-02 14:07:13 +0200

    added views for different roles +action +viewmodels

--- MiaBoard/Controllers/TestController.cs
+++ MiaBoard/Controllers/TestController.cs
@@ -1,13 +1,269 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
-
+using System.Data.SqlClient;
+using MiaBoard.Models;
+using MiaBoard.ViewModels;
 namespace MiaBoard.Controllers
 {
     public class TestController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public ActionResult ViewUser(int id)
+        {
+            if (id == 0)
+                return HttpNotFound();
+
+            var model = new DashboardViewViewUserViewModel();
+
+            model.Dashlets = new List<Dashlet>();
+            model.DataSources = new List<DataSource>();
+
+            model.DashletsSqlResult = new Dictionary<int, string>();
+
+            model.DashletsFirstCol = new List<Dashlet>();
+            model.DashletsSecondCol = new List<Dashlet>();
+            model.DashletsThirdCol = new List<Dashlet>();
+
+            model.DataSources = db.DataSources.ToList();
+            model.DashboardList = db.Dashboards.ToList();
+            model.Dashboard = db.Dashboards.SingleOrDefault(d => d.Id == id);
+            //model.Dashlets = db.Dashlets.Where(lam => lam.DashboardId == id ).OrderBy(lam => lam.Position).ToList();
+            model.Dashlets = db.Dashlets.Include(m => m.DataSource).Where(d => d.DashboardId == id).OrderBy(d => d.Position).ToList();
+
+
+            foreach (var dashlet in model.Dashlets)
+            {
+                //switch (dashlet.DataSource.Type)
+                //{
+                //    case "MSSQL":
+                //        try
+                //        {
+                //            using (SqlConnection connection = new SqlConnection())
+                //            {
+                //                connection.ConnectionString = dashlet.DataSource.ConnectionString;
+                //                connection.Open();
+
+                //                SqlCommand command = new SqlCommand(dashlet.Sql, connection);
+
+                //                using (SqlDataReader reader = command.ExecuteReader())
+                //                {
+                //                    if (reader.Read())
+                //                    {
+                //                        viewDashboardViewModel.DashletsSqlResult.Add(dashlet.Id, reader[0].ToString());
+                //                    }
+                //                }
+
+                //                connection.Close();
+                //            }
+                //        }
+                //        catch
+                //        {
+                //            viewDashboardViewModel.DashletsSqlResult.Add(dashlet.Id,"SQL queary is incorrect!");
+                //            //continue;
+                //            //return Content("Invalid Connection to Database in Dashhlet: " + dashlet.Id);
+                //        }
+                //        break;
+                //    default:
+                //        viewDashboardViewModel.DashletsSqlResult.Add(dashlet.Id, "Type of Database is unknown!");
+                //        //continue;
+                //        //return Content("Invalid Type of Database in Dashhlet: " + dashlet.Id);
+                //        break;
+                //}
+
+                switch (dashlet.Column)
+                {
+                    case 1:
+                        model.DashletsFirstCol.Add(dashlet);
+                        break;
+                    case 2:
+                        model.DashletsSecondCol.Add(dashlet);
+                        break;
+                    case 3:
+                        model.DashletsThirdCol.Add(dashlet);
+                        break;
+                    default:
+                        Console.WriteLine("Toggle is broke. It's equal to " + dashlet.Column);
+                        break;
+                }
+
+            }
+
+            return View("~/Views/Dashboards/ViewUser.cshtml", model);
+        }
+
+        public ActionResult ViewUserReadOnly(int id)
+        {
+            if (id == 0)
+                return HttpNotFound();
+
+            var model = new DashboardViewViewUserReadOnlyViewModel();
+
+            model.Dashlets = new List<Dashlet>();
+            model.DataSources = new List<DataSource>();
+
+            model.DashletsSqlResult = new Dictionary<int, string>();
+
+            model.DashletsFirstCol = new List<Dashlet>();
+            model.DashletsSecondCol = new List<Dashlet>();
+            model.DashletsThirdCol = new List<Dashlet>();
+
+            model.DataSources = db.DataSources.ToList();
+            model.DashboardList = db.Dashboards.ToList();
+            model.Dashboard = db.Dashboards.SingleOrDefault(d => d.Id == id);
+            //model.Dashlets = db.Dashlets.Where(lam => lam.DashboardId == id ).OrderBy(lam => lam.Position).ToList();
+            model.Dashlets = db.Dashlets.Include(m => m.DataSource).Where(d => d.DashboardId == id).OrderBy(d => d.Position).ToList();
+
+
+            foreach (var dashlet in model.Dashlets)
+            {
+                //switch (dashlet.DataSource.Type)
+                //{
+                //    case "MSSQL":
+                //        try
+                //        {
+                //            using (SqlConnection connection = new SqlConnection())
+                //            {
+                //                connection.ConnectionString = dashlet.DataSource.ConnectionString;
+                //                connection.Open();
+
+                //                SqlCommand command = new SqlCommand(dashlet.Sql, connection);
+
+                //                using (SqlDataReader reader = command.ExecuteReader())
+                //                {
+                //                    if (reader.Read())
+                //                    {
+                //                        viewDashboardViewModel.DashletsSqlResult.Add(dashlet.Id, reader[0].ToString());
+                //                    }
+                //                }
+
+                //                connection.Close();
+                //            }
+                //        }
+                //        catch
+                //        {
+                //            viewDashboardViewModel.DashletsSqlResult.Add(dashlet.Id,"SQL queary is incorrect!");
+                //            //continue;
+                //            //return Content("Invalid Connection to Database in Dashhlet: " + dashlet.Id);
+                //        }
+                //        break;
+                //    default:
+                //        viewDashboardViewModel.DashletsSqlResult.Add(dashlet.Id, "Type of Database is unknown!");
+                //        //continue;
+                //        //return Content("Invalid Type of Database in Dashhlet: " + dashlet.Id);
+                //        break;
+                //}
+
+                switch (dashlet.Column)
+                {
+                    case 1:
+                        model.DashletsFirstCol.Add(dashlet);
+                        break;
+                    case 2:
+                        model.DashletsSecondCol.Add(dashlet);
+                        break;
+                    case 3:
+                        model.DashletsThirdCol.Add(dashlet);
+                        break;
+                    default:
+                        Console.WriteLine("Toggle is broke. It's equal to " + dashlet.Column);
+                        break;
+                }
+
+            }
+
+            return View("~/Views/Dashboards/ViewUserReadOnly.cshtml", model);
+        }
+
+        public ActionResult ViewCompanyAdmin(int id)
+        {
+            if (id == 0)
+                return HttpNotFound();
+
+            var model = new DashboardViewViewCompanyAdminViewModel();
+
+            model.Dashlets = new List<Dashlet>();
+            model.DataSources = new List<DataSource>();
+
+            model.DashletsSqlResult = new Dictionary<int, string>();
+
+            model.DashletsFirstCol = new List<Dashlet>();
+            model.DashletsSecondCol = new List<Dashlet>();
+            model.DashletsThirdCol = new List<Dashlet>();
+
+            model.DataSources = db.DataSources.ToList();
+            model.DashboardList = db.Dashboards.ToList();
+            model.Dashboard = db.Dashboards.SingleOrDefault(d => d.Id == id);
+            //model.Dashlets = db.Dashlets.Where(lam => lam.DashboardId == id ).OrderBy(lam => lam.Position).ToList();
+            model.Dashlets = db.Dashlets.Include(m => m.DataSource).Where(d => d.DashboardId == id).OrderBy(d => d.Position).ToList();
+
+
+            foreach (var dashlet in model.Dashlets)
+            {
+                //switch (dashlet.DataSource.Type)
+                //{
+                //    case "MSSQL":
+                //        try
+                //        {
+                //            using (SqlConnection connection = new SqlConnection())
+                //            {
+                //                connection.ConnectionString = dashlet.DataSource.ConnectionString;
+                //                connection.Open();
+
+                //                SqlCommand command = new SqlCommand(dashlet.Sql, connection);
+
+                //                using (SqlDataReader reader = command.ExecuteReader())
+                //                {
+                //                    if (reader.Read())
+                //                    {
+                //                        viewDashboardViewModel.DashletsSqlResult.Add(dashlet.Id, reader[0].ToString());
+                //                    }
+                //                }
+
+                //                connection.Close();
+                //            }
+                //        }
+                //        catch
+                //        {
+                //            viewDashboardViewModel.DashletsSqlResult.Add(dashlet.Id,"SQL queary is incorrect!");
+                //            //continue;
+                //            //return Content("Invalid Connection to Database in Dashhlet: " + dashlet.Id);
+                //        }
+                //        break;
+                //    default:
+                //        viewDashboardViewModel.DashletsSqlResult.Add(dashlet.Id, "Type of Database is unknown!");
+                //        //continue;
+                //        //return Content("Invalid Type of Database in Dashhlet: " + dashlet.Id);
+                //        break;
+                //}
+
+                switch (dashlet.Column)
+                {
+                    case 1:
+                        model.DashletsFirstCol.Add(dashlet);
+                        break;
+                    case 2:
+                        model.DashletsSecondCol.Add(dashlet);
+                        break;
+                    case 3:
+                        model.DashletsThirdCol.Add(dashlet);
+                        break;
+                    default:
+                        Console.WriteLine("Toggle is broke. It's equal to " + dashlet.Column);
+                        break;
+                }
+
+            }
+
+            return View("~/Views/Dashboards/ViewCompanyAdmin.cshtml", model);
+        }
+
         //
         // GET: /Test/
         public ActionResult Index()
@@ -29,5 +285,5 @@ namespace MiaBoard.Controllers
         {
             return View();
         }
-	}
+    }
 }
---
+++ MiaBoard/ViewModels/DashboardViewViewCompanyAdminViewModel.cs
@@ -0,0 +1,21 @@
+﻿using MiaBoard.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MiaBoard.ViewModels
+{
+    public class DashboardViewViewCompanyAdminViewModel
+    {
+        public Dashboard Dashboard { get; set; }
+        public List<Dashboard> DashboardList { get; set; }
+        public List<DataSource> DataSources { get; set; }
+        public IDictionary<int, string> DashletsSqlResult { get; set; }
+        public Dashlet Dashlet { get; set; }
+        public List<Dashlet> Dashlets { get; set; }
+        public List<Dashlet> DashletsFirstCol { get; set; }
+        public List<Dashlet> DashletsSecondCol { get; set; }
+        public List<Dashlet> DashletsThirdCol { get; set; }
+    }
+}
